5791aaecb804af776cbd8cd39fed388fcbb068ba
Artem <ar.lubimov@gmail.com>
2024-07-31 21:29:57 +0300

    ---

--- clientMysql/index/admin.py
+++ clientMysql/index/admin.py
@@ -1,12 +1,13 @@
 from django.contrib.admin import ModelAdmin, register
 from .models import *
 
+
 @register(PktRecordLog)
 class PktRecordLogAdmin(ModelAdmin):
     pass
 
 
-
 @register(Pktreader)
 class PktreaderAdmin(ModelAdmin):
     list_display = ("mac_addr", "ip_addr", "time")
+    list_filter = ('mac_addr', 'ip_addr')
