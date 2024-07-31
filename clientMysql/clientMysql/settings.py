f75469827047929de1a96004caa1f23da764720d
Artem <ar.lubimov@gmail.com>
2024-07-31 20:35:56 +0300

    static

--- clientMysql/clientMysql/settings.py
+++ clientMysql/clientMysql/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = Path(BASE_DIR).joinpath("static").parent
+STATIC_ROOT = Path(BASE_DIR).parent.joinpath("static")
 
 # STATICFILES_DIRS = [
 #     Path(BASE_DIR).joinpath("static"),
