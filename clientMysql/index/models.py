1bcc2bacf58b4261ad7f8c3a14882a2bea5758db
Artem <ar.lubimov@gmail.com>
2024-07-31 21:18:38 +0300

    ---

--- clientMysql/index/models.py
+++ clientMysql/index/models.py
@@ -13,6 +13,8 @@ class Pktreader(models.Model):
     mac_addr = models.CharField("mac_addr", max_length=32, null=False, blank=False, help_text="mac адрес pc")
     ip_addr = models.CharField("id_addr", max_length=32, null=False, blank=False, help_text="ip адрес pc")
     time = models.DateTimeField("time", null=False, help_text="Время обнуружения pc")
+    def __str__(self):
+        return f"компьютер ({self.id}) {self.mac_addr}:{self.ip_addr}"
     class Meta:
         verbose_name = "компьютер"
         verbose_name_plural = "компьютеры"
