206cce068fabfae8833f66711c149793f5577cdf
Artem <ar.lubimov@gmail.com>
2024-08-02 19:24:18 +0300

    ---

--- clientMysql/rest/urls.py
+++ clientMysql/rest/urls.py
@@ -25,6 +25,6 @@ schema_view = get_schema_view(
 urlpatterns = [
     path('', include(router.urls)),
     path('rest-api.json', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger'),
+    path('swagger', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger'),
 
 ]
