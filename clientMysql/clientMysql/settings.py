f5cf4b7f7ee975caee7188ff74e6d23b7c02f94b
Artem <ar.lubimov@gmail.com>
2024-08-02 21:07:18 +0300

    ---

--- clientMysql/clientMysql/settings.py
+++ clientMysql/clientMysql/settings.py
@@ -34,6 +34,8 @@ INSTALLED_APPS = [
     'rest',
     'rest_framework',
     'drf_yasg',
+    'adminlte3',
+    'adminlte3_theme',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
