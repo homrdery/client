013384bba09d69bba3512874edba5986513d130c
Artem <ar.lubimov@gmail.com>
2024-08-02 20:52:44 +0300

    ---

--- clientMysql/index/views.py
+++ clientMysql/index/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-
+from .models import Pktreader
 
 # Create your views here.
 
@@ -8,4 +8,9 @@ def index(request):
     return render(request, "index/index.html")
 
 def computers(request):
-    return render(request, "index/computers.html")
+    items = Pktreader.objects.all().order_by("time")
+    params = {
+        "items": items,
+        "title": f"всего компов"
+    }
+    return render(request, "index/computers.html", params)
