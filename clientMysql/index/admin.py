32648d0fc323312e16a82c81c963857e511c22d8
Artem <ar.lubimov@gmail.com>
2024-08-05 16:35:12 +0300

    ---

--- clientMysql/index/admin.py
+++ clientMysql/index/admin.py
@@ -9,7 +9,7 @@ class PktRecordLogAdmin(ModelAdmin):
 
 @register(Pktreader)
 class PktreaderAdmin(ModelAdmin):
-    pass
-    # list_display = ("mac_addr", "ip_addr", "time")
+    # pass
+    list_display = ("mac_addr", "ip_addr", "time")
     # list_filter = ('mac_addr', 'ip_addr')
     # search_fields = ('mac_addr', 'ip_addr')
