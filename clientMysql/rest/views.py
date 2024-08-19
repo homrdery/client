96d1a3dedb0f8840d425c52a45106d22b59de9e3
Artem <ar.lubimov@gmail.com>
2024-08-19 17:53:01 +0300

    ВАЖНО

--- clientMysql/rest/views.py
+++ clientMysql/rest/views.py
@@ -36,7 +36,7 @@ class PktreaderViewSet(viewsets.ModelViewSet):
 
 
 def page(request):
-    items = PktReader.objects.all().order_by("time")
+    items = Pktreader.objects.all().order_by("time")
     params = {
         "items": items,
         "title": f"всего компов"
