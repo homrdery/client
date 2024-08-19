f18aae85b5d42ef6895c3e73145829d58a4ead79
Artem <ar.lubimov@gmail.com>
2024-08-19 17:17:59 +0300

    ВАЖНО

--- clientMysql/index/urls.py
+++ clientMysql/index/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 from django.contrib import admin
-
+import rest
 from . import views
 
 urlpatterns = [
-    path('', views.computers),
+    path('', rest.views.page),
     path('computers', views.computers),
 ]
