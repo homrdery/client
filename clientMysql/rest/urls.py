de196d63fbe010bdd02dc1e2993a548a3b1d9b29
Artem <ar.lubimov@gmail.com>
2024-08-19 17:27:21 +0300

    ВАЖНО

--- clientMysql/rest/urls.py
+++ clientMysql/rest/urls.py
@@ -24,10 +24,9 @@ schema_view = get_schema_view(
     public=True,
     permission_classes=[permissions.AllowAny, ],
 )
-
+# include(router.urls)
 urlpatterns = [
-    path('',  include(router.urls)),
-    path('page', views.page),
+    path('', views.page),
     path('rest-api.json', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('swagger', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger'),
 ]
