229760d240a3deda70f94ee699613d31a272bc7a
Artem <ar.lubimov@gmail.com>
2024-08-21 19:52:31 +0300

    ВАЖНО

--- clientMysql/rest/views.py
+++ clientMysql/rest/views.py
@@ -3,14 +3,20 @@ from django.shortcuts import render
 # Create your views here.
 from django.contrib.auth.models import Group, User
 from rest_framework import permissions, viewsets
+from rest_framework.response import Response
+from rest_framework.status import HTTP_200_OK
 
 from .serializers import  PktRecordLogSerializer, PktreaderSerializer
 from index.models import PktRecordLog, Pktreader
 
-class PktRecordLogViewSet(viewsets.ViewSet):
+class PktRecordLogViewSet(viewsets.ModelViewSet):
     queryset = PktRecordLog.objects.all().order_by('time')
     serializer_class = PktRecordLogSerializer
     permission_classes = [permissions.IsAuthenticated]
+    @action(detail=False, methods=["post",], url_path=r'bulk_post/)')
+    def bulk_post(self, request):
+
+        return Response({}, status=HTTP_200_OK)
 
 class PktreaderViewSet(viewsets.ModelViewSet):
     queryset = Pktreader.objects.all().order_by('time')
