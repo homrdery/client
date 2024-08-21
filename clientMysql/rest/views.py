54c396bccc33cc4399e739d31831e1ecb78d1592
Artem <ar.lubimov@gmail.com>
2024-08-21 19:38:50 +0300

    ВАЖНО

--- clientMysql/rest/views.py
+++ clientMysql/rest/views.py
@@ -4,27 +4,10 @@ from django.shortcuts import render
 from django.contrib.auth.models import Group, User
 from rest_framework import permissions, viewsets
 
-from .serializers import GroupSerializer, UserSerializer, PktRecordLogSerializer, PktreaderSerializer
+from .serializers import  PktRecordLogSerializer, PktreaderSerializer
 from index.models import PktRecordLog, Pktreader
 
-class UserViewSet(viewsets.ModelViewSet):
-    """
-    API endpoint that allows users to be viewed or edited.
-    """
-    queryset = User.objects.all().order_by('-date_joined')
-    serializer_class = UserSerializer
-    permission_classes = [permissions.IsAuthenticated]
-
-
-class GroupViewSet(viewsets.ModelViewSet):
-    """
-    API endpoint that allows groups to be viewed or edited.
-    """
-    queryset = Group.objects.all().order_by('name')
-    serializer_class = GroupSerializer
-    permission_classes = [permissions.IsAuthenticated]
-
-class PktRecordLogViewSet(viewsets.ModelViewSet):
+class PktRecordLogViewSet(viewsets.ViewSet):
     queryset = PktRecordLog.objects.all().order_by('time')
     serializer_class = PktRecordLogSerializer
     permission_classes = [permissions.IsAuthenticated]
