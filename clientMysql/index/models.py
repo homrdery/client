b9a1d5fec6035e5ff93eed60aee87f9afb43b53e
Artem <ar.lubimov@gmail.com>
2024-08-21 19:30:30 +0300

    ВАЖНО

--- clientMysql/index/models.py
+++ clientMysql/index/models.py
@@ -1,9 +1,9 @@
 from django.db import models
 
 class PktRecordLog(models.Model):
-    type = models.IntegerField("type", verbose_name="тип", null=False, help_text="Тип записи")
-    data = models.JSONField("data", null=False, help_text="Данные пакета")
-    time = models.DateTimeField("time", null=False, help_text="Время добавления")
+    type = models.IntegerField("Тип", null=False, help_text="Тип записи")
+    data = models.JSONField("Данные", null=False, help_text="Данные пакета")
+    time = models.DateTimeField("Время", null=False, help_text="Время добавления")
     def __str__(self):
         return f"пакет ({self.id}) {self.data}:{self.time}"
     class Meta:
