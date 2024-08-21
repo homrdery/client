c6759ad4924bfb36b13e79a9e1c3bde017f07b4b
Artem <ar.lubimov@gmail.com>
2024-08-21 19:24:55 +0300

    ВАЖНО

--- clientMysql/index/models.py
+++ clientMysql/index/models.py
@@ -4,6 +4,8 @@ class PktRecordLog(models.Model):
     type = models.IntegerField("type", null=False, help_text="Тип записи")
     data = models.JSONField("data", null=False, help_text="Данные пакета")
     time = models.DateTimeField("time", null=False, help_text="Время добавления")
+    def __str__(self):
+        return f"пакет ({self.id}) {self.data}:{self.time}"
     class Meta:
         verbose_name = "пакет"
         verbose_name_plural = "пакеты"
