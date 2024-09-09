0aa1d91070960a9d487ae1490fe123abef748093
Artem <ar.lubimov@gmail.com>
2024-09-09 17:43:50 +0300

    ВАЖНО

--- clientMysql/index/views.py
+++ clientMysql/index/views.py
@@ -31,4 +31,4 @@ def NeoWeb(request):
         "names": names,
         "title": f"всего компов"
     }
-    return render(request, "logs.html", params)
+    return render(request, "NeoWeb.html", params)
