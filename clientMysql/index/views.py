0bd014ce50de4a01fcd35d16fb4b6252743047f8
Artem <ar.lubimov@gmail.com>
2024-09-09 17:31:36 +0300

    ВАЖНО

--- clientMysql/index/views.py
+++ clientMysql/index/views.py
@@ -23,5 +23,5 @@ def computers(request):
         "items": items,
         "title": f"всего компов"
     }
-    return render(request, "templates/index/page.html")
+    return render(request, "index/page.html")
 
