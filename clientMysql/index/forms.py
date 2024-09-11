2c0111453ffc7dc1f5d0e77cd8b1d19bdc859837
Artem <ar.lubimov@gmail.com>
2024-09-11 20:39:24 +0300

    ВАЖНО

--- clientMysql/index/forms.py
+++ clientMysql/index/forms.py
@@ -10,7 +10,7 @@ class addForm(forms.ModelForm):
         super().__init__(*args, **kwargs)
         self.helper = FormHelper()
         self.helper.form_method = 'post'
-        self.helper.form_action = 'Addr/addPost/'
+        self.helper.form_action = ''
         self.helper.add_input(Submit('submit', 'Отправить', css_class='btn btn-primary float-end'))
 
     class Meta:
