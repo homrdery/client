cd3d33ed35b1212b1225457a5255c98e94c97864
Artem <ar.lubimov@gmail.com>
2024-09-11 19:55:44 +0300

    ВАЖНО

--- clientMysql/index/forms.py
+++ clientMysql/index/forms.py
@@ -1,9 +1,15 @@
 from django import forms
 from .models import worker
 import crispy_forms
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Submit, Hidden, Button, Layout, Fieldset, Field, HTML, Reset, Row, Column
+
 
 class addForm(forms.ModelForm):
-
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_method = 'post'
 
     class Meta:
         model = worker
