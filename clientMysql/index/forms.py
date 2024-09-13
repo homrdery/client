89e0029090a0bb800772a6d3fafefccef3da9391
Artem <ar.lubimov@gmail.com>
2024-09-13 20:38:18 +0300

    ВАЖНО

--- clientMysql/index/forms.py
+++ clientMysql/index/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from .models import worker
 from django.forms import HiddenInput
-import crispy_forms
+from crispy_forms.bootstrap import Modal
 from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Submit, Hidden, Button, Layout, Fieldset, Field, HTML, Reset, Row, Column
 
@@ -13,7 +13,7 @@ class addForm(forms.ModelForm):
         self.helper.form_method = 'post'
         self.helper.form_action = '/Addr/addPost/'
         self.helper.add_input(Submit('submit', 'Отправить', css_class='btn btn-primary float-end'))
-        # self.helper.layout = Layout(Fieldset('name', 'mac_addr'))
+        self.helper.layout = Layout(Modal(Field('name'), Button("submit", "Оправить", css_class='btn btn-primery float-end'), title='РАботаееет'))
         self.fields['mac_addr'].widget = HiddenInput()
 
 
