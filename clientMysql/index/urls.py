5862da63f4659a9799bcb5d90492547706ffa214
Artem <ar.lubimov@gmail.com>
2024-09-13 19:45:42 +0300

    ВАЖНО

--- clientMysql/index/urls.py
+++ clientMysql/index/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path('mainpage.html', views.computers),
     path('logs.html', views.logs),
     path('Addr.html', views.addr),
-    path('logs.html', views.add)
+    path('logs.html', views.formadd)
     # path('Addr/addPost/', views.addPost)
 ]
