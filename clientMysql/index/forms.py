e5cab11713e66bd8291f84c2ad41a716d16d86af
Artem <ar.lubimov@gmail.com>
2024-09-16 20:18:45 +0300

    ВАЖНО

--- clientMysql/index/forms.py
+++ clientMysql/index/forms.py
@@ -29,19 +29,20 @@ class addForm(forms.ModelForm):
         self.helper.layout = Layout(Modal(Field('name'), Field('mac_addr'),Field('action'), Submit("submit", "Оправить", css_class='btn btn-primery float-end'),  css_id="addForm", title='РАботаееет'))
 
 
-
     class Meta:
         model = worker
         fields = ('name', 'mac_addr')
 
 
-# class addFormAddr(forms.ModelForm):
-#     action = forms.CharField(widget=forms.HiddenInput(), initial="sub", required=True)
-#     def __init__(self, *args, **kwargs):
-#         super().__init__(*args, **kwargs)
-#         self.helper = FormHelper()
-#         self.helper.form_method = 'post'
-#         self.helper.form_action = ''
-#         self.helper.layout = Layout(Modal(Field('name', 'mac_addr', select='mac_addr' 'action'), Submit("submit", "Оправить", css_class='btn btn-primery float-end'),  css_id="addForm", title='РАботаееет'))
-#         self.fields['mac_addr'].widget = HiddenInput()
-#
+class addFormAddr(forms.ModelForm):
+    action = forms.CharField(widget=forms.HiddenInput(), initial="sub", required=True)
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_method = 'post'
+        self.helper.form_action = ''
+        self.helper.layout = Layout(Modal(Field('name', 'mac_addr','action'), Submit("submit", "Оправить", css_class='btn btn-primery float-end'),  css_id="addForm", title='РАботаееет'))
+
+    class Meta:
+        model = worker
+        fields = ('name', 'mac_addr')
