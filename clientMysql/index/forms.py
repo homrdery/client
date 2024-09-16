2d7436d2c4c628d9f6e83c2ace917a3011a85249
Artem <ar.lubimov@gmail.com>
2024-09-16 20:40:24 +0300

    ВАЖНО

--- clientMysql/index/forms.py
+++ clientMysql/index/forms.py
@@ -36,12 +36,14 @@ class addForm(forms.ModelForm):
 
 class addFormAddr(forms.ModelForm):
     action = forms.CharField(widget=forms.HiddenInput(), initial="subAddr", required=True)
+    mac_addr = forms.ChoiceField(choices=list_mac_addr)
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.helper = FormHelper()
         self.helper.form_method = 'post'
         self.helper.form_action = ''
-        self.helper.layout = Layout(Modal(Field('name'), Field('mac_addr'),Field('action'), Submit("submit", "Оправить", css_class='btn btn-primery float-end'),  css_id="addForm", title='РАботаееет'))
+        self.helper.layout = Layout(Modal(Field('name'), Field('mac_addr'), Field('action'), Submit("submit", "Оправить", css_class='btn btn-primery float-end'),  css_id="addForm", title='РАботаееет'))
 
     class Meta:
         model = worker
