b79410eaf8c230784359f8894c9436fb9144a32b
Artem <ar.lubimov@gmail.com>
2024-09-16 19:51:48 +0300

    ВАЖНО

--- clientMysql/index/forms.py
+++ clientMysql/index/forms.py
@@ -11,14 +11,14 @@ def list_mac_addr():
     return box
 class addForm(forms.ModelForm):
     action = forms.CharField(widget=forms.HiddenInput(), initial="sub", required=True)
-
+    mac_addr = forms.ChoiceField(choices=list_mac_addr)
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.helper = FormHelper()
         self.helper.form_method = 'post'
         self.helper.form_action = ''
         self.helper.layout = Layout(Modal(Field('name'), Field('mac_addr'),Field('action'), Submit("submit", "Оправить", css_class='btn btn-primery float-end'),  css_id="addForm", title='РАботаееет'))
-        self.fields['mac_addr'].widget = ChoiceField(choices=list_mac_addr)
+
 
 
     class Meta:
