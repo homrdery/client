569db8cb682ef8c82934f85b9d5b44cc0beee4de
Artem <ar.lubimov@gmail.com>
2024-09-16 19:44:36 +0300

    ВАЖНО

--- clientMysql/index/forms.py
+++ clientMysql/index/forms.py
@@ -17,16 +17,13 @@ class addForm(forms.ModelForm):
         self.helper = FormHelper()
         self.helper.form_method = 'post'
         self.helper.form_action = ''
-        self.helper.layout = Layout(Modal(Field('name'), Field('mac_addr'), Field('data'), Field('action'), Submit("submit", "Оправить", css_class='btn btn-primery float-end'),  css_id="addForm", title='РАботаееет'))
+        self.helper.layout = Layout(Modal(Field('name'), Field('mac_addr'),Field('action'), Submit("submit", "Оправить", css_class='btn btn-primery float-end'),  css_id="addForm", title='РАботаееет'))
         self.fields['mac_addr'].widget = ChoiceField(choices=list_mac_addr)
 
 
     class Meta:
         model = worker
-        fields = ('name', 'mac_addr', 'data')
-        widgets = {
-            'mac_addr': ChoiceField(choices=boxMac_addr)
-        }
+        fields = ('name', 'mac_addr')
 
 
 # class addFormAddr(forms.ModelForm):
