cfc87408b1ae1bf639d1736498b6cde6cecd5423
Artem <ar.lubimov@gmail.com>
2024-09-18 18:39:52 +0300

    ВАЖНО

--- static/Index/JSscripts/scripts.js
+++ static/Index/JSscripts/scripts.js
@@ -13,12 +13,12 @@ function getform () {
 
 function deladdr( e, dt, node, config )  {
     if (confirm('Are you sure you want to delete this item?')) {
-        var id = table.$('tr.selected')[0].id;
+        var id = $('#myTable').DataTable.$('tr.selected')[0].id;
         console.log('Delete user id ='+id);
         $("#Form").load("/getform.html?action=delAddr&id="+id, function(responseTxt, statusTxt, jqXHR)
         {
             if(statusTxt == 'success'){
-                    $('#addFormdel').modal();
+                    $('#addFormdel').submit();
                 }
                 if(statusTxt == 'error'){
                     alert('Error: ' + jqXHR.status + ' ' + jqXHR.statusText);
