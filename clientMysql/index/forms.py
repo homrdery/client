af7f911b0703305f5b7bbd1d98cab25bce00d856
Artem <ar.lubimov@gmail.com>
2024-09-18 19:08:54 +0300

    ВАЖНО

--- clientMysql/index/forms.py
+++ clientMysql/index/forms.py
@@ -55,7 +55,6 @@ class delFormAddr(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.helper = FormHelper()
-        self.helper.form_action = ''
         self.helper.layout = Layout(Modal(Field('mac_addr'),Field('action'), Submit("delete", "Удалить", css_class='btn btn-primery float-end'),  css_id="addFormdel", title='РАботаееет'))
 
     class Meta:
