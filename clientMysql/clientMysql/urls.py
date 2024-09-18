778d147a2e8e876f689747d29a5c41d43ae21edb
Artem <ar.lubimov@gmail.com>
2024-09-18 22:05:28 +0300

    ВАЖНО

--- clientMysql/clientMysql/urls.py
+++ clientMysql/clientMysql/urls.py
@@ -19,7 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path("", include("index.urls")),
-    path('dir/', include("directory.urls")),
+    path('directory/', include("directory.urls")),
     # path("templates/logs.html", include("logs"))
     path("rest/", include("rest.urls")),
     path('admin/', admin.site.urls),
