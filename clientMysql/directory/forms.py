5b2d8a130043afab1fd8f32c199fac4e83649e9d
Artem <ar.lubimov@gmail.com>
2024-09-18 22:17:32 +0300

    ВАЖНО

--- clientMysql/directory/forms.py
+++ clientMysql/directory/forms.py
@@ -12,6 +12,21 @@ logger = logging.getLogger(APPNAME)
 
 
 
+class addForm(forms.ModelForm):
+    action = forms.CharField(widget=forms.HiddenInput(), initial="sub", required=True)
+
+    # mac_addr = forms.CharField(widget=forms.Select(choices=list_mac_addr))
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_method = 'post'
+        self.helper.form_action = ''
+        self.helper.layout = Layout(Modal(Field('name'), Field('mac_addr'),Field('action'), Submit("submit", "Оправить", css_class='btn btn-primery float-end'),  css_id="addForm", title='обавить новую запись'))
+
+
+    class Meta:
+        model = dirAddr
+        fields = ('name', 'mac_addr', 'data')
 
 class delFormAddr(forms.ModelForm):
     action = forms.CharField(widget=forms.HiddenInput(), initial="delAddr", required=True)
